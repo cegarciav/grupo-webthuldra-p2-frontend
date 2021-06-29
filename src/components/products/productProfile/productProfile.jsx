9b2250d90257d3a665b4f7dbfb2ee58f74671ad9
cegarciav <cegarcia2@uc.cl>
2021-06-29 19:15:30 -0400

    feat: add product's complete profile

--- src/components/products/productProfile/productProfile.jsx
+++ src/components/products/productProfile/productProfile.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import {
+  Link,
+  useParams,
+} from 'react-router-dom';
 
 const ProductProfile = () => {
   const { productId } = useParams();
+  const productInfo = {
+    id: productId,
+    name: 'Product 1',
+    stock: 100,
+    price: 500,
+    unit: null,
+    storeId: '207ff996-34aa-4fb6-90ed-ea57a74d8642',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSsfKHi3iw8M4r6es253TBy0sH_i4D9WSrt23oHNSB5eygGMStKIkKETHNxd2DGhOUHo4&usqp=CAU',
+  };
   return (
     <main>
-      <h1>
-        { `Product ${productId}'s profile`}
-      </h1>
+      <section className="return-link">
+        <Link
+          to={`/stores/${productInfo.storeId}`}
+        >
+          <i className="material-icons">arrow_back</i>
+          Ir a la tienda
+        </Link>
+      </section>
+      <section className="product-card">
+        <figure>
+          <img src={productInfo.image} alt={productInfo.name} />
+        </figure>
+        <article className="product-info">
+          <h1>{productInfo.name}</h1>
+          <h3>{`$${productInfo.price} ${productInfo.unit || 'c/u'}`}</h3>
+          <h4>{`Disponibles: ${productInfo.stock} ${productInfo.unit || 'unidades'}`}</h4>
+        </article>
+      </section>
     </main>
   );
 };
