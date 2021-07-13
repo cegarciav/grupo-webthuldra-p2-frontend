c3ead75e83714b05d72b773aaee3772215281e9e
cegarciav <cegarcia2@uc.cl>
2021-07-13 03:16:41 -0400

    feat: allow sending messages in a deal

--- src/components/deals/dealProfile/dealProfile.jsx
+++ src/components/deals/dealProfile/dealProfile.jsx
@@ -1,14 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import {
+  Formik,
+  Form,
+  Field,
+} from 'formik';
+import * as Yup from 'yup';
 import { useParams } from 'react-router-dom';
 import ErrorsModal from '../../common/errorsModal';
-import { apiGet } from '../../../apiService';
+import { apiGet, apiPost } from '../../../apiService';
 
 const DealProfile = () => {
   const { storeId, dealId } = useParams();
   const [deal, setDeal] = useState(null);
+  const [myInfo, setMyInfo] = useState(null);
+  const [amICustomer, setAmICustomer] = useState(false);
   const [messages, setMessages] = useState([]);
+  const [updatedMessages, setUpdatedMessages] = useState(false);
   const [loading, setLoading] = useState(false);
   const [errors, setErrors] = useState(null);
+
+  const messageValidation = Yup.object({
+    text: Yup.string()
+      .required(''),
+  });
 
   useEffect(async () => {
     setLoading(true);
@@ -19,6 +33,7 @@ const DealProfile = () => {
       const messagesResponse = await apiGet(`/deals/${dealId}/messages`);
       if (messagesResponse.data && messagesResponse.statusCode === 200) {
         setMessages(messagesResponse.data);
+        setUpdatedMessages(true);
       } else if (messagesResponse.type === 'response' && messagesResponse.errors) {
         setErrors(messagesResponse.errors);
       } else {
@@ -30,6 +45,51 @@ const DealProfile = () => {
       setErrors([dealResponse]);
     }
   }, []);
+
+  useEffect(async () => {
+    if (deal) {
+      setLoading(true);
+      const myInfoResponse = await apiGet('/users/me');
+      setLoading(false);
+      if (myInfoResponse.data && myInfoResponse.statusCode === 200) {
+        setMyInfo(myInfoResponse.data);
+        if (deal.customerId === myInfoResponse.data.id) {
+          setAmICustomer(true);
+        }
+      } else if (myInfoResponse.type === 'response' && myInfoResponse.errors) {
+        setErrors(myInfoResponse.errors);
+      } else {
+        setErrors([myInfoResponse]);
+      }
+    }
+  }, [deal]);
+
+  useEffect(async () => {
+    if (!updatedMessages) {
+      const messagesResponse = await apiGet(`/deals/${dealId}/messages`);
+      if (messagesResponse.data && messagesResponse.statusCode === 200) {
+        setMessages(messagesResponse.data);
+        setUpdatedMessages(true);
+      } else if (messagesResponse.type === 'response' && messagesResponse.errors) {
+        setErrors(messagesResponse.errors);
+      } else {
+        setErrors([messagesResponse]);
+      }
+    }
+  }, [updatedMessages]);
+
+  const sendMessage = async (values) => {
+    setLoading(true);
+    const newMessageResponse = await apiPost(`/deals/${deal.id}/messages`, values);
+    setLoading(false);
+    if (newMessageResponse.data && newMessageResponse.statusCode === 201) {
+      setUpdatedMessages(false);
+    } else if (newMessageResponse.type === 'response' && newMessageResponse.errors) {
+      setErrors(newMessageResponse.errors);
+    } else {
+      setErrors([newMessageResponse]);
+    }
+  };
 
   if (loading) {
     return (
@@ -46,36 +106,57 @@ const DealProfile = () => {
     const productsList = deal.products
       .map((product) => (
         <li key={product.id}>
-          <h2>{product.name}</h2>
-          <p>{`Cantidad: ${product.purchase.amount} ${product.unit || 'c/u'}`}</p>
+          <figure className="product-in-deal">
+            <img src={product.picture || '/default_product_picture.png'} alt={product.name} />
+            <figcaption>
+              {`${product.name}. Cantidad: ${product.purchase.amount} ${product.unit || 'c/u'}`}
+            </figcaption>
+          </figure>
         </li>
       ));
 
     const messagesList = messages
-      .map((message) => (
-        <li key={message.id}>
-          <h2>{message.sender}</h2>
-          <p>{message.text}</p>
-        </li>
-      ));
+      .map((message) => {
+        const isMyMessage = (amICustomer && message.sender === 'customer')
+          || (!amICustomer && message.sender === 'store');
+        return (
+          <li key={message.id} className={isMyMessage ? 'my-message' : 'other-message'}>
+            <p>{message.text}</p>
+          </li>
+        );
+      });
 
     return (
       <main>
         <button type="button">Editar deal</button>
         <button type="button">Eliminar deal</button>
-        <section className="store-main-profile">
-          <aside className="store-info">
+        <section className="deal-main-profile">
+          <aside className="deal-info">
             <h1>Productos a comprar</h1>
             <ul>
               {productsList}
             </ul>
           </aside>
-          <section className="products-in-store">
+          <section className="messages-in-deal">
             <h1>Mensajes</h1>
             <ul>
-              {messagesList}
+              {(myInfo && messagesList) ? messagesList : null}
             </ul>
-            <input type="text" />
+            <Formik
+              initialValues={{ text: '' }}
+              validationSchema={messageValidation}
+              onSubmit={sendMessage}
+            >
+              {() => (
+                <Form className="form-user-inputs">
+                  <Field
+                    name="text"
+                    placeholder="Escribe un mensaje"
+                    type="text"
+                  />
+                </Form>
+              )}
+            </Formik>
           </section>
         </section>
       </main>
