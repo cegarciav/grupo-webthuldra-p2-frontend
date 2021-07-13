7fc8df48f6ffbb5776ad3f8d02f99846cbec1833
cegarciav <cegarcia2@uc.cl>
2021-07-13 14:13:24 -0400

    feat: create comments in store profile

--- src/components/comments/commentsList/commentsList.jsx
+++ src/components/comments/commentsList/commentsList.jsx
@@ -1,32 +1,70 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+} from 'formik';
+import * as Yup from 'yup';
 import CommentProfileSummary from '../commentProfile/commentProfileSummary';
 import ErrorsModal from '../../common/errorsModal';
-import { apiGet } from '../../../apiService';
+import { apiGet, apiPost } from '../../../apiService';
 import './commentsList.css';
 
 function CommentsList({ storeId, reviewerId }) {
   const [comments, setComments] = useState([]);
   const [loading, setLoading] = useState(false);
   const [errors, setErrors] = useState(null);
+  const [updatedComments, setUpdatedComments] = useState(false);
+
+  const commentInitialValues = {
+    text: '',
+    grade: undefined,
+  };
+
+  const commentValidation = Yup.object({
+    text: Yup.string()
+      .required('Debes ingresar al menos un caracter'),
+    grade: Yup.number()
+      .integer('Debes ingresar una cantidad válida')
+      .min(0, 'La nota no puede ser menor a 0')
+      .max(5, 'La nota no puede ser mayor a 5'),
+  });
+
+  const createComment = async (values) => {
+    setLoading(true);
+    const newCommentResponse = await apiPost(`/stores/${storeId}/comments`, values);
+    setLoading(false);
+    if (newCommentResponse.data && newCommentResponse.statusCode === 201) {
+      setUpdatedComments(false);
+    } else if (newCommentResponse.type === 'response' && newCommentResponse.errors) {
+      setErrors(newCommentResponse.errors);
+    } else {
+      setErrors([newCommentResponse]);
+    }
+  };
 
   useEffect(async () => {
-    setLoading(true);
-    let commentsResponse;
-    if (storeId) {
-      commentsResponse = await apiGet(`/stores/${storeId}/comments`);
-    } else if (reviewerId) {
-      commentsResponse = await apiGet(`/users/${reviewerId}/comments`);
+    if (!updatedComments) {
+      setLoading(true);
+      let commentsResponse;
+      if (storeId) {
+        commentsResponse = await apiGet(`/stores/${storeId}/comments`);
+      } else if (reviewerId) {
+        commentsResponse = await apiGet(`/users/${reviewerId}/comments`);
+      }
+      setLoading(false);
+      setUpdatedComments(true);
+      if (commentsResponse.data && commentsResponse.statusCode === 200) {
+        setComments(commentsResponse.data);
+      } else if (commentsResponse.type === 'response' && commentsResponse.errors) {
+        setErrors(commentsResponse.errors);
+      } else {
+        setErrors([commentsResponse]);
+      }
     }
-    setLoading(false);
-    if (commentsResponse.data && commentsResponse.statusCode === 200) {
-      setComments(commentsResponse.data);
-    } else if (commentsResponse.type === 'response' && commentsResponse.errors) {
-      setErrors(commentsResponse.errors);
-    } else {
-      setErrors([commentsResponse]);
-    }
-  }, []);
+  }, [updatedComments]);
 
   if (loading) {
     return (
@@ -59,7 +97,42 @@ function CommentsList({ storeId, reviewerId }) {
       <ul className="comments-list">
         {commentComponents}
       </ul>
-      {storeId ? <a role="button" href="/">Escribir un comentario</a> : null}
+      {storeId
+        ? (
+          <section className="create-comment-section">
+            <Formik
+              initialValues={commentInitialValues}
+              validationSchema={commentValidation}
+              onSubmit={createComment}
+            >
+              {({ isValid, dirty }) => (
+                <Form className="form-new-comment">
+                  <section className="form-field-100">
+                    <Field name="text" placeholder="Comentar..." />
+                    <ErrorMessage className="form-error" name="text" component="p" />
+                  </section>
+                  <section className="form-field-100">
+                    <Field
+                      name="grade"
+                      type="number"
+                      placeholder="Nota"
+                      min="0"
+                      max="5"
+                    />
+                    <ErrorMessage className="form-error" name="grade" component="p" />
+                  </section>
+                  <button
+                    type="submit"
+                    disabled={!(isValid && dirty)}
+                    className={!(isValid && dirty) ? 'button-disabled' : ''}
+                  >
+                    Enviar comentario
+                  </button>
+                </Form>
+              )}
+            </Formik>
+          </section>
+        ) : null}
     </main>
   );
 }
